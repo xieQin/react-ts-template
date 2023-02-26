63443609ffa59f2360fa049539cb51ea6f6b3a43
cherish <xieqin1991@gmail.com>
2023-02-26 22:22:27 +0800

    refactor: design patterns page

--- src/views/DesignPatterns/index.tsx
+++ src/views/DesignPatterns/index.tsx
@@ -1,34 +1,32 @@
+import { DesignPatterns } from "@/models";
+
 import AbstractFactoryPage from "./AbstractFactory";
 import BuilderPage from "./Builder";
 import FactoryPage from "./Factory";
 import PrototypePage from "./Prototype";
 import SingletonPage from "./Singleton";
 
+const DesignPatternsPageConfigs = {
+  [DesignPatterns.Factory]: FactoryPage,
+  [DesignPatterns.AbstractFactory]: AbstractFactoryPage,
+  [DesignPatterns.Builder]: BuilderPage,
+  [DesignPatterns.Prototype]: PrototypePage,
+  [DesignPatterns.Singleton]: SingletonPage,
+};
+
 export default function DesignPatternsPage() {
   return (
     <>
       <h3>Design Patterns</h3>
       <ul>
-        <li>
-          <h4>Factory</h4>
-          <FactoryPage />
-        </li>
-        <li>
-          <h4>Abstract Factory</h4>
-          <AbstractFactoryPage />
-        </li>
-        <li>
-          <h4>Builder</h4>
-          <BuilderPage />
-        </li>
-        <li>
-          <h4>Prototype</h4>
-          <PrototypePage />
-        </li>
-        <li>
-          <h4>Singleton</h4>
-          <SingletonPage />
-        </li>
+        {Object.keys(DesignPatternsPageConfigs).map(key => (
+          <li key={key}>
+            <h4>{key}</h4>
+            {DesignPatternsPageConfigs[
+              key as keyof typeof DesignPatternsPageConfigs
+            ]()}
+          </li>
+        ))}
       </ul>
     </>
   );
